6366d8783eddd57c5091270ff76e6f50dee1abf5
luckymichaln <michalnowak.contact@gmail.com>
2019-11-25 20:47:43 +0100

    add Athlete Profile component

---
+++ src/components/AthleteProfile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="athlete-profile">
+    <section>
+      <h1>{{ $prismic.richTextAsPlain(data.hero.fullName) }}</h1>
+      <div>
+        <img
+          :src="data.hero.heroImage.url"
+          :srcset="`${data.hero.heroImage.url} 1x, ${data.hero.heroImage.url} 2x`"
+        />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Athlete.vue
+++ src/pages/Athlete.vue
@@ -2,7 +2,7 @@
   <div>
     <transition name="fade" mode="out-in">
       <div v-if="isContentLoaded">
-        {{  }}
+        <AthleteProfile :data="athleteData"/>
       </div>
       <div class="loader" v-if="!isContentLoaded"/>
     </transition>
@@ -11,6 +11,7 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex'
+import AthleteProfile from '../components/AthleteProfile'
 
 export default {
   name: 'athlete-profile',
@@ -30,36 +31,8 @@ export default {
     }, 1000)
   },
 
-  methods: {
-    sliderResolver (data) {
-      return data.map(el => {
-        return el.slider_image.url ? el.slider_image : el.slider_link
-      })
-    },
-
-    getContent (uid) {
-      return this.$prismic.client.getByUID('athlete_page', uid).then((document) => {
-        if (document) {
-          const { achievements, birthday, age, coach, full_name, hero_achievements, hero_image, hero_social_media, hometown, motto, quote, slider } = document.data
-
-          this.hero.image = hero_image
-          this.hero.name = full_name
-          this.hero.age = age
-          this.hero.achievements = hero_achievements
-          this.hero.socialMedia = hero_social_media
-
-          this.bio.birthday = birthday
-          this.bio.coach = coach
-          this.bio.hometown = hometown
-          this.bio.motto = motto
-          this.bio.quote = quote
-
-          this.media.slider = this.sliderResolver(slider)
-        } else {
-          this.$router.push('/not-found')
-        }
-      });
-    }
+  components: {
+    AthleteProfile
   }
 }
 </script>
